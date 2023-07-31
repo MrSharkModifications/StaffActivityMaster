c72f018f1664b7d0abe5b196a324687d7f564c83
TonyStarkTheDev <94570419+TonyStarkTheDev@users.noreply.github.com>
2023-07-31 22:29:19 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {
+    RoleList = { 
+        ['🦺 Management | '] = {1125462066250121307, 2, nil},
+        ['🦺 HeadAdmin | '] = {1125291759455445026, 17, nil},
+        ['🦺 SeniorAdmin | '] = {1125291759455445025, 3, nil},
+        ['🦺 Admin |'] =  {1125291759455445024, 3, nil},
+        ['🦺 Head Moderator |'] =  {887518674607562803, 3, nil},
+        ['🦺 Senior Moderator |'] =  {1125291759421898885, 3, nil},
+        ['🦺 Moderator |'] =  {1125291759421898884, 3, nil},
+        ['🦺 Trail Moderator |']  =  {1125291759421898883, 3, nil},
+    },
+    CLIENT_UPDATE_INTERVAL_SECONDS = 3, -- How frequently should the blips on the map update??
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'ThatGuyShark'
+description 'StaffActivityMaster'
+version '1.0'
+
+client_scripts {
+	'client.lua',
+    'EmergencyBlips/cl_emergencyblips.lua',
+}
+
+server_scripts {
+	'config.lua',
+	"server.lua",
+    'EmergencyBlips/sv_emergencyblips.lua',
+}
